241022f5d5461f58c27838d03830d7fd219316af
Ricardo Paschoal Junior <83047488+rpaschoaljr@users.noreply.github.com>
2025-12-09 22:11:53 +0000

    add ex3

---
+++ Exercicios/Ex03.py
@@ -0,0 +1,15 @@
+#Ler 2 variaveis
+#a
+#b
+
+#e exibir a variavel a 2x
+#e exibir a variavel b 5x
+#Tudo na mesma linha
+#Por exemplo
+#Se a = "a"
+#Se b = "b"
+#imprimir "aabbbbb"
+
+a = input("Digite a primeira variavel: ")
+b = input("Digite a segunda variavel: ")
+print(f'{a*2}{b*5}')
