2d4370ab5ae71d18b0d8ac381ab29de7e98fe842
Ricardo Paschoal Junior <83047488+rpaschoaljr@users.noreply.github.com>
2025-12-10 00:29:50 +0000

    add exercicio 4

---
+++ Exercicios/Ex04.py
@@ -0,0 +1,17 @@
+# Escreva um programa que leia 2 numeros inteiros e exiba da seguinte forma
+# a = 2
+# b = 5
+
+# Resultado:
+# 2x5 = 10
+# 2+5 = 7
+# 2-5 = -3
+# 2/5 = 0.4
+
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+print(f"{a}x{b}={a*b}")
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
+print(f"{a}/{b}={a/b}")
