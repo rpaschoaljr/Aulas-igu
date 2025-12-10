9fb1f6596efae1d22c2d95119b6e32008361ade0
Ricardo Paschoal Junior <83047488+rpaschoaljr@users.noreply.github.com>
2025-12-10 01:05:12 +0000

    Add exercicio 5

---
+++ Exercicios/Ex05.py
@@ -0,0 +1,7 @@
+# ano_atual = 2025
+# ano_nascimento = LER O VALOR DO TECLADO
+ano_atual = 2025
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+
+
+print(f"A pessoa tem {ano_atual - ano_nascimento} anos de idade")
