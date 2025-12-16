9c714ea309e4e584193961b997af628eea82e3e9
Ricardo Paschoal Junior <83047488+rpaschoaljr@users.noreply.github.com>
2025-12-16 00:48:24 +0000

    add ex07

---
+++ Exercicios/Ex07.py
@@ -0,0 +1,10 @@
+lista_numeros = [15,20,571]
+a = lista_numeros[0]
+b = lista_numeros[1]
+c = lista_numeros[2]
+
+#Resto do codigo
+print(f'Soma de todos os numeros - {a + b + c}')
+print(f'Multiplicação de todos os numeros - {a * b * c}')
+print(f'Divisão de todos os numeros - {a / b / c}')
+print(f"Quadrado de cada numero - {a**2}, {b**2}, {c**2}")
